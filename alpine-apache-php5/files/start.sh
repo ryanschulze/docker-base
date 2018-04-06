9702cf2d1690fdb05781f9c5e08d02d630281916
Ryan Schulze <ryan@dopefish.de>
2018-04-06 16:51:03 -0500

    initial commit

---
+++ alpine-apache-php5/files/start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# execute any pre-init scripts, useful for images based on this image
+for file in /opt/utils/pre-init.d/*sh; do
+	if [[ -e "${file}" ]]; then
+		printf "[i] pre-init.d - processing %s\n" "${file}"
+		. "${file}"
+	fi
+done
+
+while read -r pattern
+do
+    sedscript+="$pattern;"
+done <<EOD
+s@#LoadModule\ rewrite_module@LoadModule\ rewrite_module@
+s@#LoadModule\ deflate_module@LoadModule\ deflate_module@
+s@#LoadModule\ expires_module@LoadModule\ expires_module@
+s@^DocumentRoot \".*@DocumentRoot \"/app/$WEBAPP_ROOT\"@g
+s@/var/www/localhost/htdocs@/app/$WEBAPP_ROOT@
+EOD
+
+sed -ie "${sedscript}" /etc/apache2/httpd.conf
+printf "\n<Directory \"/app${WEBAPP_ROOT}\">\n\tAllowOverride All\n</Directory>\n" >> /etc/apache2/httpd.conf
+
+# execute any pre-exec scripts, useful for images based on this image
+for file in /opt/utils/pre-exec.d/*sh; do
+	if [[ -e "${file}" ]]; then
+		printf "[i] pre-exec.d - processing %s\n" "${file}"
+		. "${file}"
+	fi
+done
+
+echo "[i] Starting daemon..."
+# run apache httpd daemon
+exec httpd -D FOREGROUND
