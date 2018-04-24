205de7def2a001c06f700377ecfdfca085d88152
Ryan Schulze <ryan@dopefish.de>
2018-04-24 12:12:10 -0500

    initial import

---
+++ alpine-lighttpd-static/files/start.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# execute any pre-init scripts, useful for images based on this image
+for file in /opt/utils/pre-init.d/*sh; do
+	if [[ -e "${file}" ]]; then
+		printf "[i] pre-init.d - processing %s\n" "${file}"
+		. "${file}"
+	fi
+done
+
+cat <<EOD >/etc/lighttpd/lighttpd.conf
+server.port          = $APP_PORT
+server.username      = "lighttpd"
+server.groupname     = "lighttpd"
+server.document-root = "/app"
+server.errorlog	     = "/dev/stdout"
+dir-listing.activate = "disable"
+index-file.names     = ( "index.html" )
+mimetype.assign      = (
+  ".html" => "text/html",
+  ".txt"  => "text/plain",
+  ".css"  => "text/css",
+  ".js"   => "application/x-javascript",
+  ".jpg"  => "image/jpeg",
+  ".jpeg" => "image/jpeg",
+  ".gif"  => "image/gif",
+  ".png"  => "image/png",
+  "" => "application/octet-stream"
+)
+static-file.exclude-extensions = ( ".fcgi", ".php", ".rb", "~", ".inc" )
+EOD
+
+if [[ "${WEBSERVER_UID}" != '' ]]; then
+	usermod -u ${WEBSERVER_UID} lighttpd
+fi
+
+# execute any pre-exec scripts, useful for images based on this image
+for file in /opt/utils/pre-exec.d/*sh; do
+	if [[ -e "${file}" ]]; then
+		printf "[i] pre-exec.d - processing %s\n" "${file}"
+		. "${file}"
+	fi
+done
+
+echo "[i] Starting daemon..."
+# run lighttpd daemon
+exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
